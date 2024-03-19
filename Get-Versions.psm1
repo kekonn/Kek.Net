614e3f4bfb76b6baf6d7b67420f7e03e1f431553
kekonn <ketsukonn@gmail.com>
2024-03-19 15:29:59 +0100

    WIP build pipeline

---
+++ Get-Versions.psm1
@@ -0,0 +1,21 @@
+function Get-Versions {
+    $versionFiles = Get-Item -Path src/**/version.yaml | Select-Object -ExpandProperty FullName
+    Write-Debug "Found the following version files:"
+    $versionFiles | ForEach-Object { $_ | Write-Debug }
+
+    $packageVersions = @{}
+    $versionFiles | ForEach-Object {
+        Write-Debug "Reading version file $_"
+        $packageName = Split-Path $_ -Parent | Split-Path -Leaf
+        $yamlVersion = (Get-Content $_ | ConvertFrom-Yaml).version
+        $semver = '{0}.{1}.{2}' -f $yamlVersion.major, $yamlVersion.minor, $yamlVersion.patch
+        $assemblyVersion = $semver + ".0"
+        
+        $packageVersions[$packageName] = @{ 'SemVer' = $semver; 'AssemblyVersion' = $assemblyVersion; 'Root' = $(Split-Path $_ -Parent)}
+    }
+    
+    $packageVersions | ConvertTo-Json | Write-Output
+}
+
+Install-Module -Name powershell-yaml -Force -AcceptLicense
+Export-ModuleMember -Function Get-Versions
