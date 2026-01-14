1366b5fc0dba64285cddfa8aa02024e95dfcfc28
Jonas Van der aa <jva@igcgroup.com>
2026-01-14 13:49:36 +0100

    Update dependencies, add more tests

---
+++ Kek.Net.Extensions.Tests/IEnumerableExtensionTests.cs
@@ -0,0 +1,26 @@
+﻿namespace Kek.Net.Extensions.Tests;
+
+public class IEnumerableExtensionTests
+{
+    [Fact]
+    public void Flatten_ShouldThrow_IfNull()
+    {
+        Assert.Throws<ArgumentNullException>(() => IEnumerableExtensions.Flatten<string>(null, null));
+        Assert.Throws<ArgumentNullException>(() => Array.Empty<string>().Flatten(null));
+        Assert.Throws<ArgumentNullException>(() => IEnumerableExtensions.Flatten<object>(null, s => []));
+    }
+
+    [Fact]
+    public void Flatten_ShouldReturn_EmptyCollectionOnEmptyCollection()
+    {
+        // Arrange
+        var empty = Array.Empty<object>();
+
+        // Act
+        var result = empty.Flatten(o => []);
+
+        // Assert
+        Assert.IsAssignableFrom<object[]>(result);
+        Assert.Empty(result);
+    }
+}
